4400ad6788b67832f8ea9479711de8bb34da191c
Sam Weimer <sweimer1@binghamton.edu>
2021-04-07 17:56:51 -0400

    submitting

---
+++ weimer_s_p2/Contestant.java
@@ -0,0 +1,41 @@
+
+public class Contestant {
+	private int totalPoints = 0;
+	private int ID;
+//	private int index;
+	
+	public Contestant(int ID, int totalPoints) {
+		this.ID = ID;
+		this.totalPoints = totalPoints;
+	}
+	
+	public int getTotalPoints() {
+		return totalPoints;
+	}
+	
+	public void setTotalPoints(int totalPoints) {
+		this.totalPoints = totalPoints;
+	}
+	
+	public void addPoints(int points) {
+		this.totalPoints += points;
+	}
+	
+	public void subtractPoints(int points) {
+		this.totalPoints -= points;
+	}
+	
+	public int getID() {
+		return ID;
+	}
+	
+	public void setID(int iD) {
+		ID = iD;
+	}
+//	public int getIndex() {
+//		return index;
+//	}
+//	public void setIndex(int index) {
+//		this.index = index;
+//	}
+}
